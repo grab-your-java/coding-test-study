1baa7e83aebce424406eee4e8068e43feb4f08d3
newbieJanghan <bjh4135@gmail.com>
2024-07-18 10:55:28 +0900

    [#8] 정렬 / Pass: SelectionSort 구현

--- newbieJanghan/day04/src/sort/SelectionSort.java
+++ newbieJanghan/day04/src/sort/SelectionSort.java
@@ -2,7 +2,7 @@ package sort;
 
 import java.util.Scanner;
 
-public class BubbleSort {
+public class SelectionSort {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -15,16 +15,16 @@ public class BubbleSort {
 		}
 		
 		// sort
-		int c = N;
-		while (c > 1) {
-			for (int i=0; i<c-1; i++) {
-				if (list[i] > list[i+1]) {
-					int temp = list[i];
-					list[i] = list[i+1];
-					list[i+1] = temp;
+		for (int i=0; i<N; i++) {
+			int minIdx = i;
+			for (int j=i+1; j<N; j++) {
+				if (list[minIdx] > list[j]) {
+					minIdx = j;
 				}
 			}
-			c--;
+			int temp = list[i];
+			list[i] = list[minIdx];
+			list[minIdx] = temp;
 		}
 		
 		// print
