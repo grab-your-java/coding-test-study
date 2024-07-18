6f40e59171af601317714fef23c4fbea860430a1
newbieJanghan <bjh4135@gmail.com>
2024-07-18 14:17:18 +0900

    [#9] 2차원배열 / 충돌 조건을 잘 떠올려서 해결

--- newbieJanghan/day04/src/magnetic/Solution.java
+++ newbieJanghan/day04/src/magnetic/Solution.java
@@ -5,36 +5,35 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for (int t = 1; t <= 10; t++) {
+		for (int t=1; t<=10; t++) {
 			int result = 0;
-			int N = 100;
-
+			int N = sc.nextInt();
+			
 			// Mapping
 			int[][] grid = new int[N][N];
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++) {
+			for (int r=0; r<N; r++) {
+				for (int c=0; c<N; c++) {
 					grid[r][c] = sc.nextInt();
 				}
 			}
-
-			// N극은 stack에 저장하고
-			// S극은 stack이 비어있지 않으면 충돌 후 stack 초기화
-			for (int c = 0; c < N; c++) {
-				int stacked = 0;
-				for (int r = 0; r < N; r++) {
+			
+			// 각 컬럼마다 로우를 순회하면서 
+			// N극을 만나면 기억한 후, S극을 만나면 충돌 카운트 올리고 기억 초기화.
+			for (int c=0; c<N; c++) {
+				boolean stacked = false;
+				for (int r=0; r<N; r++) {
 					// N극
-					if (grid[r][c] == 1) {
-						stacked++;
-						continue;
-					}
+					if (grid[r][c] == 1 && !stacked) {
+						stacked = true;
+					} 
 					// S극
-					if (grid[r][c] == 2 && stacked > 0) {
+					else if (grid[r][c] == 2 && stacked) {
 						result++;
-						stacked = 0;
+						stacked = false;
 					}
 				}
 			}
-
+			
 			System.out.println("#" + t + " " + result);
 		}
 	}
