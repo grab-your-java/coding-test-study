e8fc296314b06ca25d589b2ab97c51ec0006c782
newbiejanghan <bjh4135@gmail.com>
2024-07-25 00:02:50 +0900

    draf

--- newbieJanghan/day08/src/mellon/Main.java
+++ newbieJanghan/day08/src/mellon/Main.java
@@ -3,54 +3,39 @@ package mellon;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int P = sc.nextInt();
-		int height = 0;
-		int width = 0;
-		int spareHeight = 0;
-		int spareWidth = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int P = sc.nextInt();
+        int height = 0;
+        int width = 0;
+        int spareArea = 0;
 
-		int[] distances = new int[4];
-		boolean alreadyDouble = false;
-		int iter = 6;
-		while (iter-- > 0) {
-			int dir = sc.nextInt();
-			int distance = sc.nextInt();
+        // when direction doubled, multiply its distance with before distance
+        // it would be spare area which should be subtracted;
+        int[] directionCounter = new int[4];
+        int lastDistance = 0;
 
-			if (distances[dir - 1] == 0) {
-				// Set height, width
-				if (dir == 3 || dir == 4) {
-					height = Math.max(height, distance);
-				} else {
-					width = Math.max(width, distance);
-				}
+        int iter = 6;
+        while (iter-- > 0) {
+            int dir = sc.nextInt();
+            int distance = sc.nextInt();
 
-				distances[dir - 1] = distance;
-				continue;
-			}
+            // case: already this direction proceeded
+            if (directionCounter[dir - 1] > 0) {
+                spareArea = lastDistance * distance;
+            } else {
+                // Set height, width
+                if (dir == 3 || dir == 4) {
+                    height = Math.max(height, distance);
+                } else {
+                    width = Math.max(width, distance);
+                }
+            }
+            lastDistance = distance;
+            directionCounter[dir - 1]++;
+        }
 
-			// Check Shape
-			if (!alreadyDouble) {
-				// when first double direction
-				if (dir == 3 || dir == 4) {
-					spareHeight = distance;
-				} else {
-					spareWidth = distance;
-				}
-
-				alreadyDouble = true;
-			} else {
-				// when first double direction
-				if (dir == 3 || dir == 4) {
-					spareHeight = distances[dir - 1];
-				} else {
-					spareWidth = distances[dir - 1];
-				}
-			}
-		}
-		
-		long area = height * width - (spareHeight * spareWidth);
-		System.out.println(P * area);
-	}
+        long area = (long) height * width - spareArea;
+        System.out.println(P * area);
+    }
 }
