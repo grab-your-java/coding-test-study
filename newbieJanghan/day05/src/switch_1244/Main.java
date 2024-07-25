24f6d25a9f43be28c54bc3e0275fde37b36e6f55
newbieJanghan <bjh4135@gmail.com>
2024-07-25 10:47:44 +0900

    [#37] 구현 / Draft

--- newbieJanghan/day05/src/switch_1244/Main.java
+++ newbieJanghan/day05/src/switch_1244/Main.java
@@ -1,6 +1,5 @@
 package switch_1244;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -22,21 +21,18 @@ public class Main {
 			case 1:
 				for (int j = 1; j <= switches.length; j++) {
 					if (j % number == 0) {
-						toggle(switches, j-1);
-						System.out.println("After toggle: " + Arrays.toString(switches));
+						toggle(switches, j - 1);
 					}
 				}
 				break;
 			// girl
 			case 2:
-				int space = number > (N / 2) ? N - number : number - 1;
-				System.out.println("Space: " + space);
-				
+				int space = Math.min(N - number, number - 1);
 				int idx = number - 1;
 				int l = idx;
 				int r = idx;
 				while (space > 0 && l >= 0 && r < N) {
-					if (switches[l-1] == switches[r+1]) {
+					if (switches[l - 1] == switches[r + 1]) {
 						l--;
 						r++;
 						space--;
@@ -44,27 +40,24 @@ public class Main {
 						break;
 					}
 				}
-				System.out.println("*** l -> r ***");
-				System.out.println("*** " + l + " -> " + r + " ***");
-				for (int j=l; j<=r; j++) {
+				for (int j = l; j <= r; j++) {
 					toggle(switches, j);
-					System.out.println("After toggle: " + Arrays.toString(switches));
 				}
 				break;
 			}
 		}
-		
+
 		// printb
 		StringBuilder sb = new StringBuilder();
-		for (int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			sb.append(switches[i] + " ");
-			if ((i+1) % 20 == 0) {
+			if ((i + 1) % 20 == 0) {
 				sb.append("\n");
 			}
 		}
 		System.out.println(sb);
 	}
-	
+
 	public static void toggle(int[] switches, int i) {
 		switches[i] = switches[i] == 1 ? 0 : 1;
 	}
