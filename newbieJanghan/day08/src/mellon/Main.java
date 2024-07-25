abacd04659b9feeca3158611de4636ae1d650078
newbieJanghan <bjh4135@gmail.com>
2024-07-25 09:45:26 +0900

    [#44] 구현 / Draft

--- newbieJanghan/day08/src/mellon/Main.java
+++ newbieJanghan/day08/src/mellon/Main.java
@@ -3,39 +3,39 @@ package mellon;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int P = sc.nextInt();
-        int height = 0;
-        int width = 0;
-        int spareArea = 0;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int P = sc.nextInt();
+		int height = 0;
+		int width = 0;
+		long smallArea = 0;
 
-        // when direction doubled, multiply its distance with before distance
-        // it would be spare area which should be subtracted;
-        int[] directionCounter = new int[4];
-        int lastDistance = 0;
+		int[] directions = new int[6];
+		int[] distances = new int[6];
+		for (int i = 0; i < 6; i++) {
+			int direction = sc.nextInt();
+			directions[i] = direction;
 
-        int iter = 6;
-        while (iter-- > 0) {
-            int dir = sc.nextInt();
-            int distance = sc.nextInt();
+			int distance = sc.nextInt();
+			distances[i] = distance;
 
-            // case: already this direction proceeded
-            if (directionCounter[dir - 1] > 0) {
-                spareArea = lastDistance * distance;
-            } else {
-                // Set height, width
-                if (dir == 3 || dir == 4) {
-                    height = Math.max(height, distance);
-                } else {
-                    width = Math.max(width, distance);
-                }
-            }
-            lastDistance = distance;
-            directionCounter[dir - 1]++;
-        }
+			if (direction == 3 || direction == 4) {
+				height = Math.max(height, distance);
+			} else {
+				width = Math.max(width, distance);
+			}
+		}
 
-        long area = (long) height * width - spareArea;
-        System.out.println(P * area);
-    }
+		// when direction doubled after one line,
+		// small area created.
+		for (int i = 0; i < 6; i++) {
+			if (directions[(i + 2) % 6] == directions[i]) {
+				smallArea = (long) distances[(i + 1) % 6] * distances[(i + 2) % 6];
+				break;
+			}
+		}
+
+		long area = (long) height * width - smallArea;
+		System.out.println(P * area);
+	}
 }
