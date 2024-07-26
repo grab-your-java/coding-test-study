def9ba1718463a77b320bd7ca6c598505b9c8db8
newbieJanghan <bjh4135@gmail.com>
2024-07-26 15:29:45 +0900

    [#49] 그리디

--- newbieJanghan/day10/src/cup_holder/Main.java
+++ newbieJanghan/day10/src/cup_holder/Main.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		// Input
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		char[] chars = sc.next().toCharArray();
 
+		// Count char 'L'
 		int lCount = 0;
 		for (char c : chars) {
 			if (c == 'L') {
 				lCount++;
 			}
 		}
+		
+		// Supposing every 'S' and pair of 'L' use left side cup-holder
+		// and also supposing every pair of 'L' needs one more cup-holder,
+		// always right side cup-holder of last man is available.
+		// Greedy Calculation = N - (count of 'L' / 2 ) + the last cup-holder 
 		int result; 
 		if (N < N + 1 - (lCount / 2)) {
 			result = N;
