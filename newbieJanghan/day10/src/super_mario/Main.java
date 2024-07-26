6450df34dc87ed6536f1499172d19423f1f6d33c
newbieJanghan <bjh4135@gmail.com>
2024-07-26 15:52:07 +0900

    [#66] 슈퍼마리오 / while true 문의 반복 사이클을 꼼꼼히 따지며 사용하자

--- newbieJanghan/day10/src/super_mario/Main.java
+++ newbieJanghan/day10/src/super_mario/Main.java
@@ -7,14 +7,15 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int sum = 0;
 
-		while (true) {
+		int counter = 10;
+		while (counter-- > 0) {
 			int temp = sum + sc.nextInt();
 			if (temp <= 100) {
 				if ((100 - sum) > (100 - temp)) {
 					sum = temp;
 				}
 			} else {
-				if (Math.abs(100 - sum) > Math.abs(100 - temp)) {
+				if (Math.abs(100 - sum) >= Math.abs(100 - temp)) {
 					sum = temp;
 				}
 				break;
