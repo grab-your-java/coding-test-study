6fe807dba41f0c013768bc1f3c05a771ac918bc6
newbieJanghan <bjh4135@gmail.com>
2024-08-02 09:55:43 +0900

    [#47] 색종이 2 / 검색한 풀이방법. 어케했누

--- newbieJanghan/day09/src/paper2/Main.java
+++ newbieJanghan/day09/src/paper2/Main.java
@@ -1,43 +1,50 @@
 package paper2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// input
 		int N = sc.nextInt();
-		int result = 0;
-
-		int[] x1List = new int[N];
-		int[] x2List = new int[N];
-		int[] y1List = new int[N];
-		int[] y2List = new int[N];
-
-		int minx1 = 101;
-		int maxx2 = 0;
-		int miny1 = 101;
-		int maxy2 = 0;
-
+		// if [100,100] is border, [100,101] would be referred.
+		// and also if [1, 1] is border, [1,0] would be referred.
+		// so make empty index for code simplicity.
+		int[][] matrix = new int[102][102];
 		for (int i = 0; i < N; i++) {
-			int ax1 = sc.nextInt();
-			int ax2 = ax1 + 10;
-			int ay1 = sc.nextInt();
-			int ay2 = ay1 + 10;
-			// 색종이 좌표 기억
-			x1List[i] = ax1;
-//			minx1 = Math.min(minx1, ax1);
-			x2List[i] = ax2;
-//			maxx2 = Math.max(maxx2, ax2);
-			y1List[i] = ay1;
-//			miny1 = Math.min(miny1, ay1);
-			y2List[i] = ay2;
-//			maxy2 = Math.max(maxy2, ay2);
-
-			// 그리기
-			// ax1, ay1 -> ax1, ay2
-			
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			for (int r = y; r < y + 10; r++) {
+				for (int c = x; c < x + 10; c++) {
+					matrix[r][c] = 1;
+				}
+			}
 		}
+//		for (int i = 99; i >= 0; i--) {
+//			System.out.println(Arrays.toString(matrix[i]));
+//		}
 
+		int result = 0;
+		for (int r = 1; r <= 100; r++) {
+			for (int c = 1; c <= 100; c++) {
+				if (matrix[r][c] == 0) {
+					continue;
+				}
+				if (matrix[r + 1][c] == 0) {
+					result++;
+				}
+				if (matrix[r][c + 1] == 0) {
+					result++;
+				}
+				if (matrix[r - 1][c] == 0) {
+					result++;
+				}
+				if (matrix[r][c - 1] == 0) {
+					result++;
+				}
+			}
+		}
 		System.out.println(result);
 	}
 }
