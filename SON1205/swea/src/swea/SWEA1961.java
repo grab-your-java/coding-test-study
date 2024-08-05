d5b1b612144bb13875d7c6c36579d0147a15aa5e
SON1205 <sonjh970226@gmail.com>
2024-08-05 17:51:12 +0900

    solve additional 6 problems

--- SON1205/swea/src/swea/SWEA1961.java
+++ SON1205/swea/src/swea/SWEA1961.java
@@ -1,7 +1,45 @@
 package swea;
 
+import java.util.Scanner;
+
 public class SWEA1961 {
 	public static void main(String[] args) {
-	
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			
+			int[][] arr = new int[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			System.out.println("#" + test_case);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(arr[N - 1 - j][i]);
+				}
+				
+				System.out.print(" ");
+				
+				for (int j = 0; j < N; j++) {
+					System.out.print(arr[N - 1 - i][N - 1 - j]);
+				}
+				
+				System.out.print(" ");
+				
+				for (int j = 0; j < N; j++) {
+					System.out.print(arr[j][N - 1 - i]);
+				}
+				
+				System.out.println();
+			}
+			
+		}
 	}
 }
