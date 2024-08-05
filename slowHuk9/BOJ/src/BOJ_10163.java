18803f4a5b057c807b13c732126c2b0c659f1891
Huk9uri <huk9uri@gmail.com>
2024-08-05 17:52:07 +0900

    10163, 14696 다시하기

---
+++ slowHuk9/BOJ/src/BOJ_10163.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_10163 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int paper = sc.nextInt();
+
+		int[][] map = new int[1001][1001];
+		for (int i = 1; i <= paper; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+
+			int width = sc.nextInt();
+			int height = sc.nextInt();
+
+			for (int j = x; j < x + height; j++) {
+				for (int k = y; k < y + width; k++) {
+					map[j][k] = i;
+				}
+			}
+		}
+
+		for (int i = 1; i <= paper; i++) {
+			int area = 0;
+			for (int j = 0; j < map.length; j++) {
+				for (int k = 0; k < map.length; k++) {
+					if (map[j][k] == i) {
+						area++;
+					}
+				}
+			}
+			System.out.println(area);
+		}
+		System.out.println("왜 안되는데 내가 뭘 놓쳤는데");
+
+	}
+
+}
---
+++ slowHuk9/BOJ/src/BOJ_13300.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BOJ_13300 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt(); // 학생 수
+		int k = sc.nextInt(); // 방에 들어갈 수 있는 최대 학생 수
+		int cnt = 0;
+		int[][] st = new int[2][6]; // [성별][학년]
+		for (int i = 0; i < n; i++) {
+			int gender = sc.nextInt(); // 0 or 1
+			int grade = sc.nextInt(); // 1~6
+			st[gender][grade - 1]++;
+		}
+
+		for (int gender = 0; gender < st.length; gender++) {
+			for (int grade = 0; grade < st[0].length; grade++) {
+				if (st[gender][grade] > 0 && st[gender][grade] <= k) {
+					cnt++;
+				}
+				if (st[gender][grade] > 0 && st[gender][grade] > k) {
+					if (st[gender][grade] % k == 0) {
+						cnt += st[gender][grade] / k;
+					} else
+						cnt += (st[gender][grade] / k) + 1;
+				}
+			}
+		}
+		System.out.println(cnt);
+
+//		for (int gender = 0; gender < st.length; gender++) {
+//			for (int grade = 0; grade < st[0].length; grade++) {
+//				System.out.print(st[gender][grade] + " ");
+//			}
+//			System.out.println();
+//		}
+
+	}
+
+}
