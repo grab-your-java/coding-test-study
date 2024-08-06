930afce4b5824e6680f599a51fd0eb70715afe15
Huk9uri <huk9uri@gmail.com>
2024-08-06 17:56:04 +0900

    0806

--- slowHuk9/BOJ/src/BOJ_11399.java
+++ slowHuk9/BOJ/src/BOJ_11399.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 
 public class BOJ_11399 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		int[] minute = new int[n];
-		for (int i = 0; i < n; i++) {
-			minute[i] = sc.nextInt();
-		}
-		Arrays.sort(minute);
-		int sum = 0;
-		for (int i = 0; i < minute.length; i++) {
-			sum += minute[i];
-			minute[i] = sum;
-		}
-		int result = 0;
-		for (int i = 0; i < minute.length; i++) {
-			result += minute[i];
-		}
-		System.out.println(result);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int[] minute = new int[n];
+        for (int i = 0; i < n; i++) {
+            minute[i] = sc.nextInt();
+        }
+        Arrays.sort(minute);
+        int sum = 0;
+        for (int i = 0; i < minute.length; i++) {
+            sum += minute[i];
+            minute[i] = sum;
+        }
+        int result = 0;
+        for (int i = 0; i < minute.length; i++) {
+            result += minute[i];
+        }
+        System.out.println(result);
+    }
 
 }
--- slowHuk9/BOJ/src/BOJ_14696.java
+++ slowHuk9/BOJ/src/BOJ_14696.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 
 public class BOJ_14696 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); // 라운드 수
-		for (int i = 1; i <= n; i++) {
-			int cnt = sc.nextInt(); // 카드의 수
-			int[] A = new int[cnt];
-			for (int j = 0; j < cnt; j++) {
-				A[j] = sc.nextInt();
-			}
-			cnt = sc.nextInt();
-			int[] B = new int[cnt];
-			for (int j = 0; j < cnt; j++) {
-				B[j] = sc.nextInt();
-			}
-			Arrays.sort(A);
-			Arrays.sort(B);
-			System.out.println(Arrays.toString(A));
-			System.out.println(Arrays.toString(B));
-			
-			
-		}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 라운드 수
+        for (int i = 1; i <= n; i++) {
+            int cnt = sc.nextInt(); // 카드의 수
+            int[] A = new int[cnt];
+            for (int j = 0; j < cnt; j++) {
+                A[j] = sc.nextInt();
+            }
+            cnt = sc.nextInt();
+            int[] B = new int[cnt];
+            for (int j = 0; j < cnt; j++) {
+                B[j] = sc.nextInt();
+            }
+            Arrays.sort(A);
+            Arrays.sort(B);
+            System.out.println(Arrays.toString(A));
+            System.out.println(Arrays.toString(B));
+            
+            
+        }
 
-	}
+    }
 
 }
