2eb6fbadb7f522983b6276d498522969c5e28f56
elsa-kim <ksh96611@naver.com>
2024-08-07 16:48:17 +0900

    [#31] 어디에 단어가 들어갈 수 있을까. 더 줄일 수 있는 방법 뭘까

---
+++ elsa-kim/swea/src/swea/D2/SWEA_1979.java
@@ -0,0 +1,48 @@
+package swea.D2;
+
+import java.util.Scanner;
+// 어디에 단어가 들어갈 수 있을까
+public class SWEA_1979 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc<=T; tc++) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			int[][] arr = new int[n][n];
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<n; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			int cnt = 0;
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<n; j++) {
+					if(arr[i][j]==1) {
+						int sum = 0;
+						while(j<n&&arr[i][j]==1) {
+							sum++;
+							j++;
+						}
+						if(sum==k) cnt++;
+					}
+				}
+			}
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<n; j++) {
+					if(arr[j][i]==1) {
+						int sum = 0;
+						while(j<n&&arr[j][i]==1) {
+							sum++;
+							j++;
+						}
+						if(sum==k) cnt++;
+					}
+					
+				}
+			}
+			System.out.println("#"+tc+" "+cnt);
+		}
+	}
+
+}
