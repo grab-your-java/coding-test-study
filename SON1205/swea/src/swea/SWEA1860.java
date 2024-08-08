4550153ab020fc135b5b12d45cb12c7821546547
SON1205 <sonjh970226@gmail.com>
2024-08-08 11:13:23 +0900

    tmp commit

--- SON1205/swea/src/swea/SWEA1860.java
+++ SON1205/swea/src/swea/SWEA1860.java
@@ -1,5 +1,45 @@
 package swea;
 
-public class SWEA1860 {
+import java.util.Scanner;
 
+public class SWEA1860 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			int K = sc.nextInt();
+			
+			int[] coming = new int[11112];
+			
+			for (int i = 0; i < N; i++) {
+				coming[sc.nextInt()]++;
+			}
+			
+			boolean possible = true;
+			int total = 0;
+			
+			for (int i = 0; i < 11112; i++) {
+				if (coming[i] == 0) {
+					continue;
+				}
+				
+				total += coming[i];
+				
+				if (i / M * K < total) {
+					possible = false;
+					break;
+				}
+			}
+			
+			if (possible) {
+				System.out.println("#" + tc + " Possible");
+			} else {
+				System.out.println("#" + tc + " Impossible");
+			}
+		}
+	}
 }
--- SON1205/swea/src/swea/SWEA7236.java
+++ SON1205/swea/src/swea/SWEA7236.java
@@ -1,5 +1,54 @@
 package swea;
 
-public class SWEA7236 {
+import java.util.Scanner;
 
+public class SWEA7236 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			
+			char[][] water = new char[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					water[i][j] = sc.next().charAt(0);
+				}
+			}
+			
+			int[] deltaX = {-1, 0, 1};
+			int[] deltaY = {-1, 0, 1};
+			
+			int max = 0;
+			
+			for (int i = 1; i < N - 1; i++) {
+				for (int j = 1; j < N - 1; j++) {
+					if (water[i][j] == 'W') {
+						int cnt = 0;
+						
+						for (int p = 0; p < deltaX.length; p++) {
+							for (int q = 0; q < deltaY.length; q++) {
+								if (water[i + deltaX[p]][j + deltaY[q]] == 'W' && (p != 1 || q != 1)) {
+									cnt++;
+								}
+							}
+						}
+						
+						if (cnt == 0) {
+							cnt = 1;
+						}
+						
+						if (cnt > max) {
+							max = cnt;
+						}
+					}
+				}
+			} 
+			
+			System.out.println("#" + tc + " " + max);
+		}
+	}
 }
