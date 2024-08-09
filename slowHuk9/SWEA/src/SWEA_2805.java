ee28719679114c942a3eab49d0db25cbda265521
Huk9uri <huk9uri@gmail.com>
2024-08-09 11:22:17 +0900

    #118 #117 #116 #101 #57 #56 #25

---
+++ slowHuk9/SWEA/src/SWEA_2805.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SWEA_2805 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int testCase = sc.nextInt();
+
+		for (int i = 1; i <= testCase; i++) {
+			int n = sc.nextInt();
+
+			int[][] map = new int[n][n];
+
+			for (int j = 0; j < map.length; j++) {
+				String s = sc.next();
+				for (int k = 0; k < map.length; k++) {
+					map[j][k] = s.charAt(k) - '0'; // 아스키코드를 활용하여 char를 정수로 변환
+				}
+			}
+
+//			for (int j = 0; j < map.length; j++) {
+//				for (int k = 0; k < map.length; k++) {
+//					System.out.print(map[j][k]);
+//				}
+//				System.out.println();
+//			}
+
+			int sum = 0;
+			for (int j = 0; j < n / 2; j++) {
+				for (int k = n / 2 - j; k <= n / 2 + j; k++) {
+					sum += map[j][k];
+				}
+			}
+
+			for (int j = n / 2; j < n / 2 + 1; j++) {
+				for (int k = 0; k < map.length; k++) {
+					sum += map[j][k];
+				}
+			}
+
+			for (int j = n / 2 + 1; j < map.length; j++) {
+				for (int k = n / 2 - (n - 1 - j); k <= n / 2 + (n - 1 - j); k++) {
+					sum += map[j][k];
+				}
+			}
+
+			System.out.println("#" + i + " " + sum);
+		}
+
+	}
+}
