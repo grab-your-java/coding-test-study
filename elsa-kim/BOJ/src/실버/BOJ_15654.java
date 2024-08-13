a28be56c78558de856090c2e310c6bafb416d7ca
sohyun kim <ksh96611@naver.com>
2024-08-14 00:08:45 +0900

    [#139]N과 M(5) : 메모리 32760 / 시간 444

---
+++ elsa-kim/BOJ/src/실버/BOJ_15654.java
@@ -0,0 +1,51 @@
+package 실버;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_15654 {
+	static int n;
+	static int m;
+	static int[] origin;
+	static int[] arr;
+	static boolean[] check;
+	static StringBuffer sb = new StringBuffer();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		origin = new int[n];
+		for(int i=0; i<n; i++) {
+			origin[i] = sc.nextInt();
+		}
+		check = new boolean[n];
+		arr = new int[m];
+		Arrays.sort(origin);
+		per(0);
+		System.out.println(sb);
+	}
+
+	private static void per(int cnt) {
+		if(cnt==m) {
+			for(int i =0; i<m; i++) {
+				sb.append(arr[i]);
+				if(i !=m-1) {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0; i<origin.length; i++) {
+			if(!check[i]) {
+				check[i] = true;
+				arr[cnt] = origin[i];
+				per(cnt+1);
+				check[i] = false;
+			}
+		}
+		
+	}
+
+}
