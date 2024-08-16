5cd26f1605dd76a3d666a331a78b859e57f4e28e
newbiejanghan <bjh4135@gmail.com>
2024-08-17 02:00:38 +0900

    [#132] 수영장 / 29,180kb 222ms: dfs 경우의 수 제어를 반복문 시작하기 전에 하자

--- newbieJanghan/day25/src/swea_1952_수영장/Solution.java
+++ newbieJanghan/day25/src/swea_1952_수영장/Solution.java
@@ -4,81 +4,69 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = Integer.parseInt(sc.nextLine());
-		for (int t = 1; t <= T; t++) {
-			int[] pricesByPlan = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			int[] visitDaysByMonth = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+        for (int t = 1; t <= T; t++) {
+            int[] pricesByPlan = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] visitDaysByMonth = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-			System.out.println("#" + t + " " + new Calculator(pricesByPlan, visitDaysByMonth).exec());
-		}
-	}
+            System.out.println("#" + t + " " + new Calculator(pricesByPlan, visitDaysByMonth).exec());
+        }
+    }
 
-	private static class Calculator {
-		private int[] pricesByPlan;
-		private int[] visitDaysByMonth;
-		private int minPrice = Integer.MAX_VALUE;
+    private static class Calculator {
+        private final int[] pricesByPlan;
+        private final int[] visitDaysByMonth;
+        private int minPrice = Integer.MAX_VALUE;
 
-		Calculator(int[] pricesByPlan, int[] visitDaysByMonth) {
-			this.pricesByPlan = pricesByPlan;
-			this.visitDaysByMonth = visitDaysByMonth;
-		}
+        Calculator(int[] pricesByPlan, int[] visitDaysByMonth) {
+            this.pricesByPlan = pricesByPlan;
+            this.visitDaysByMonth = visitDaysByMonth;
+        }
 
-		int exec() {
-			dfs(new int[12], 0, 0);
-			return minPrice;
-		}
+        int exec() {
+            dfs(0, 0);
+            return minPrice;
+        }
 
-		void dfs(int[] plansByMonth, int monthDepth, int planIterator) {
-			// calculate
-			if (monthDepth == plansByMonth.length) {
-				int totalPrice = 0;
-//				System.out.println(Arrays.toString(plansByMonth));
+        void dfs(int month, int accPrice) {
+            // calculate
+            if (month >= 12) {
+                minPrice = Math.min(minPrice, accPrice);
+                return;
+            }
 
-				for (int month = 0; month < plansByMonth.length; month++) {
-					int days = visitDaysByMonth[month];
-					if (days == 0) {
-						continue;
-					}
-					int plan = plansByMonth[month];
-					int price = pricesByPlan[plan];
+            if (visitDaysByMonth[month] == 0) {
+                dfs(month + 1, accPrice);
+            } else {
+                for (int plan = 0; plan < 4; plan++) {
+                    int price = pricesByPlan[plan];
+                    int days = visitDaysByMonth[month];
 
-					switch (plan) {
-					case 0: // day by day
-						totalPrice += price * days;
-						break;
-					case 1: // for a month
-						totalPrice += price;
-						break;
-					case 2: // for three months
-						totalPrice += price;
-						month += 2;
-						break;
-					default: // case 3. for a year
-						totalPrice = price;
-						break;
+                    int assumedPrice = calPriceByPlan(plan, price, days);
+                    int assumedMonthAfter = getMonthAfterByPlan(plan);
 
-					}
-				}
-//				System.out.println("total: " + totalPrice);
-				minPrice = Math.min(minPrice, totalPrice);
+                    if (accPrice + assumedPrice < minPrice) {
+                        dfs(month + assumedMonthAfter, accPrice + assumedPrice);
+                    }
+                }
+            }
+        }
 
-				return;
-			}
+        private int calPriceByPlan(int plan, int price, int days) {
+            return plan == 0 ? price * days : price;
+        }
 
-			if (visitDaysByMonth[monthDepth] == 0) {
-				dfs(plansByMonth, monthDepth + 1, planIterator);
-			} else {
-				for (int plan = planIterator; plan < pricesByPlan.length; plan++) {
-//					System.out.println("----------");
-//					System.out.println("*** depth: " + monthDepth);
-//					System.out.println("*** plan iterating from: " + planIterator + " to: " + plan);
-					plansByMonth[monthDepth] = plan;
-					dfs(plansByMonth, monthDepth + 1, planIterator);
-				}
-			}
+        private int getMonthAfterByPlan(int plan) {
+            if (plan == 2) {
+                return 3;
+            } else if (plan == 3) {
+                return 12;
+            } else {
+                return 1;
+            }
+        }
 
-		}
-	}
+    }
 }
