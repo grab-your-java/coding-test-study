8844fe339525cf522c996d574f89ffdd09529826
sohyun kim <ksh96611@naver.com>
2024-08-17 13:30:21 +0900

    [#149] N-Queen : 메모리 21124 / 시간 146

--- elsa-kim/forA/src/SWEA/SWEA_2806_N_Queen.java
+++ elsa-kim/forA/src/SWEA/SWEA_2806_N_Queen.java
@@ -20,47 +20,38 @@ public class SWEA_2806_N_Queen {
 			check = new boolean[N];
 			list = new ArrayList<>();
 			ans = 0;
+
 			per(0);
-			for (int i = 0; i < list.size(); i++) {
-				for (int j = 0; j < list.size(); j++) {
-					checkList(list.get(i), list.get(j));
-				}
-			}
+
 			System.out.println("#" + tc + " " + ans);
 		}
 	}
 
 	private static void per(int cnt) {
 		if (cnt == N) {
-			list.add(arr);
+			ans++;
 			return;
 		}
+
 		for (int i = 0; i < N; i++) {
-			if (!check[i]) {
+			if (!check[i] && checkPosition(cnt, i)) {
+
 				check[i] = true;
 				arr[cnt] = i;
 				per(cnt + 1);
 				check[i] = false;
+
 			}
 		}
 
 	}
 
-	private static void checkList(int[] a, int[] b) {
-		boolean[] checkSum = new boolean[2 * N - 1];
-		boolean[] checkDif = new boolean[2 * N + 1];
-		for (int i = 0; i < list.size(); i++) {
-			if (checkSum[a[i] + b[i]]) {
-				return;
-			}
-			if (checkDif[b[i] - a[i] + N - 1]) {
-				return;
-			}
-			checkDif[b[i] - a[i] + N - 1] = true;
-			checkSum[a[i] + b[i]] = true;
+	private static boolean checkPosition(int a, int b) {
+		for (int i = 0; i < a; i++) {
+			if (Math.abs(i - a) == Math.abs(arr[i] - b))
+				return false;
 		}
-
-		ans++;
+		return true;
 	}
 
 }
