231fdda754b76ce5505b6621ce2f094c863a164a
Huk9uri <huk9uri@gmail.com>
2024-08-19 21:34:48 +0900

    [#34] 파리퇴치 1 / 24,600 kb  156 ms

---
+++ slowHuk9/SWEA/src/SWEA_2001/Solution.java
@@ -0,0 +1,59 @@
+package SWEA_2001;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void fillMap(int[][] map, Scanner sc) {
+		for (int j = 0; j < map.length; j++) {
+			for (int k = 0; k < map.length; k++) {
+				map[j][k] = sc.nextInt();
+			}
+		}
+	}
+
+	public static void killFly(int[][] map, int N, int M, int[] count) {
+		int index = 0;
+		for (int k = 0; k < N - M + 1; k++) {
+			for (int j = 0; j < N - M + 1; j++) {
+				int sum = 0;
+				for (int r = 0; r < M; r++) {
+					for (int l = 0; l < M; l++) {
+						sum += map[j + r][k + l];
+					}
+				}
+				count[index++] = sum;
+			}
+		}
+	}
+
+	public static int maxFly(int[] count) {
+		Arrays.sort(count);
+		return count[count.length - 1];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+
+		for (int i = 1; i <= testCase; i++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+
+			int[][] map = new int[N][N];
+
+			fillMap(map, sc); // 파리 채우기
+
+			int[] count = new int[(N - M + 1) * (N - M + 1)];
+			killFly(map, N, M, count); // 죽인 파리 마릿수 구하기
+
+			int result = maxFly(count);
+
+			System.out.println("#" + i + " " + result);
+		}
+
+	}
+}
