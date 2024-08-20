298c4dd5619e861532405e93d68286d3403a336d
Huk9uri <huk9uri@gmail.com>
2024-08-20 11:25:06 +0900

    [#108] 안경이 없어 / 56,396 kb 283 ms

---
+++ slowHuk9/SWEA/src/SWEA_7272/test.java
@@ -0,0 +1,15 @@
+package SWEA_7272;
+
+public class test {
+	public static void main(String[] args) {
+		char ch = 'E';
+		int num = (int) ch;
+		System.out.println(num);
+		char ch2 = 'M';
+		int num2 = (int) ch2;
+		System.out.println(num2);
+		char ch3= 'C';
+		int num3 = (int) ch3;
+		System.out.println(num3);
+	}
+}
