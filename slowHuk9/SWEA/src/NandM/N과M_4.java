9739bf478fde939863ca39704de6ad96b547d977
Huk9uri <huk9uri@gmail.com>
2024-09-09 01:17:11 +0900

    [#134] [#136] [#137] [#138] [#139] [#140] [#141] [#142] A형 대비 순열, 조합 연습겸 N과M 다시해보기

---
+++ slowHuk9/SWEA/src/NandM/N과M_4.java
@@ -0,0 +1,41 @@
+package NandM;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N과M_4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+
+		int[] num = new int[N + 1];
+		int[] output = new int[M];
+
+		for (int i = 1; i < num.length; i++) {
+			num[i] = i;
+		}
+		permutation(0, M, output, num, 1, sb);
+		System.out.println(sb.toString());
+
+	}
+
+	static void permutation(int depth, int M, int[] output, int[] num, int start, StringBuilder sb) {
+		if (depth == M) {
+			for (int i : output) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = start; i < num.length; i++) {
+			output[depth] = num[i];
+			permutation(depth + 1, M, output, num, i, sb);
+		}
+
+	}
+}
