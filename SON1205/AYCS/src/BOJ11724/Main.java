3db6def86add80952af1c0b439efbd6938dc2cba
SON1205 <sonjh970226@gmail.com>
2024-09-24 16:57:00 +0900

    update #217 BOJ11724 (333400 kb / 1656 ms)

---
+++ SON1205/AYCS/src/BOJ11724/Main.java
@@ -0,0 +1,49 @@
+package BOJ11724;
+
+import java.util.*;
+
+public class Main {
+	static int result;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int V = sc.nextInt();
+		int E = sc.nextInt();
+		
+		List<List<Integer>> adjList = new ArrayList<>();
+		
+		for (int i = 0; i <= V; i++) {
+			adjList.add(new ArrayList<>());
+		}
+		
+		boolean[] visited = new boolean[V + 1];
+		
+		for (int i = 0; i < E; i++) {
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+			
+			adjList.get(u).add(v);
+			adjList.get(v).add(u);
+		}
+		
+		for (int i = 1; i <= V; i++) {
+			if (!visited[i]) {
+				result++;
+				dfs(adjList, visited, i);
+			}
+		}
+		
+		System.out.println(result);
+	}
+	
+	static void dfs(List<List<Integer>> adjList, boolean[] visited, int index) {
+		visited[index] = true;
+		
+		for (int i = 0; i < adjList.get(index).size(); i++) {
+			if (!visited[adjList.get(index).get(i)]) {
+				dfs(adjList, visited, adjList.get(index).get(i));
+			}
+		}
+	}
+}
