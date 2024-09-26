839394836a703b896f4696aaff573845951b18d3
newbieJanghan <bjh4135@gmail.com>
2024-09-26 17:24:42 +0900

    [#343] BOJ 10816 숫자 카드 2 / 시간초과 52%

--- newbieJanghan/day44/src/BOJ_10816/Main2.java
+++ newbieJanghan/day44/src/BOJ_10816/Main2.java
@@ -3,22 +3,16 @@ package BOJ_10816;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-		}
 
+		int N = Integer.parseInt(sc.nextLine());
+		int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		Arrays.sort(arr);
 
-		int M = sc.nextInt();
-		int[] keys = new int[M];
-		for (int i = 0; i < M; i++) {
-			keys[i] = sc.nextInt();
-		}
+		int M = Integer.parseInt(sc.nextLine());
+		int[] keys = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
 		StringBuilder sb = new StringBuilder();
 		for (int key : keys) {
@@ -37,7 +31,7 @@ public class Main {
 				}
 				cnt++;
 			}
-			
+
 			sb.append(cnt).append(" ");
 		}
 
