02f76779a04d528308deefda9f0e054e93988e07
newbieJanghan <bjh4135@gmail.com>
2024-10-10 21:47:03 +0900

    [#333] BOJ 1208 / 89704kb 508ms / hashmap 이용

--- newbieJanghan/day43/src/BOJ_1208/Main.java
+++ newbieJanghan/day43/src/BOJ_1208/Main.java
@@ -1,66 +1,51 @@
 package BOJ_1208;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-	static int N, S;
-	static int[] nums;
-	static int nTotal = 0, pTotal = 0;
-	static int boundary, answer;
+    static int N, S;
+    static int[] nums;
+    static Map<Integer, Long> sumCnt;
+    static int answer;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		S = sc.nextInt();
-		nums = new int[N];
-		for (int i = 0; i < N; i++) {
-			int num = sc.nextInt();
-			if (num < 0) {
-				nTotal += num;
-			} else {
-				pTotal += num;
-			}
-			nums[i] = num;
-		}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        S = sc.nextInt();
+        nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
 
-		Arrays.sort(nums);
+        sumCnt = new HashMap<>();
+        leftSubset(0, 0);
+        rightSubset(N / 2, 0);
 
-		for (int i = 0; i < N; i++) {
-			if (nums[i] > 0) {
-				boundary = i;
-				break;
-			}
-		}
+        if (S == 0) {
+            answer -= 1;
+        }
+        System.out.println(answer);
+    }
 
-		collect(0, 0);
-		collect(0, nums[0]);
-		
-		if (answer > 0) {
-			answer--; // 공집합
-		}
-		
-		System.out.println(answer);
-	}
+    static void leftSubset(int idx, int sum) {
+        if (idx == N / 2) {
+            sumCnt.put(sum, sumCnt.getOrDefault(sum, 0L) + 1L);
+            return;
+        }
 
-	static void collect(int idx, int sum) {
-		if (idx == N || sum > S) {
-			return;
-		}
+        leftSubset(idx + 1, sum);
+        leftSubset(idx + 1, sum + nums[idx]);
+    }
 
-		if (sum == S) {
-			answer++;
-			return;
-		}
+    static void rightSubset(int idx, int sum) {
+        if (idx == N) {
+            answer += sumCnt.getOrDefault(S - sum, 0L);
+            return;
+        }
 
-		if (sum + pTotal < S) {
-			collect(boundary, 0);
-			collect(boundary, nums[boundary]);
-		}
-
-		else {
-			collect(idx + 1, sum);
-			collect(idx + 1, sum + nums[idx]);
-		}
-	}
+        rightSubset(idx + 1, sum);
+        rightSubset(idx + 1, sum + nums[idx]);
+    }
 }
