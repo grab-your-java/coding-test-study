5b81dafb60d567a7bd4391b6200e5936f4314e16
elsa-kim <ksh96611@naver.com>
2024-10-17 10:45:00 +0900

    [#308] 조합 0의 개수 : 17780kb, 192ms

--- elsa-kim/BOJ/src/실버/BOJ_2004.java
+++ elsa-kim/BOJ/src/실버/BOJ_2004.java
@@ -7,33 +7,48 @@ public class BOJ_2004 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		int[][] list = new int[n + 1][2];
-		int k, cnt = 0;
 
-		for (int i = 1; i < list.length; i++) {
-			list[i] = list[i - 1].clone();
-			if (i % 2 == 0) {
-				k = i;
-				while (k % 2 == 0) {
-					cnt++;
-					k /= 2;
-				}
-				list[i][0] += cnt;
-				cnt = 0;
-			}
-			if (i % 5 == 0) {
-				k = i;
-				while (k % 5 == 0) {
-					cnt++;
-					k /= 5;
-				}
-				list[i][1] += cnt;
-				cnt = 0;
-			}
+		// 메모리 초과 코드
+//		int[][] list = new int[n + 1][2];
+//		int k, cnt = 0;
+//
+//		for (int i = 1; i < list.length; i++) {
+//			list[i] = list[i - 1].clone();
+//			if (i % 2 == 0) {
+//				k = i;
+//				while (k % 2 == 0) {
+//					cnt++;
+//					k /= 2;
+//				}
+//				list[i][0] += cnt;
+//				cnt = 0;
+//			}
+//			if (i % 5 == 0) {
+//				k = i;
+//				while (k % 5 == 0) {
+//					cnt++;
+//					k /= 5;
+//				}
+//				list[i][1] += cnt;
+//				cnt = 0;
+//			}
+//
+//		}
 
+		// 수정코드
+		System.out.println(Math.min(getAns(n, 2) - getAns(m, 2) - getAns(n - m, 2),
+				getAns(n, 5) - getAns(m, 5) - getAns(n - m, 5)));
+	}
+
+	private static int getAns(int n, int num) {
+		int cnt = 0;
+		while (n >= num) {
+			cnt += n / num;
+			n /= num;
 		}
 
-		System.out.println(Math.min(list[n][0] - list[m][0] - list[n - m][0], list[n][1] - list[m][1] - list[n - m][1]));
+		return cnt;
+
 	}
 
 }
