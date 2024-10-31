448bd4c181cfb4fc31d93ce85ab3e7eea46e0a10
Huk9uri <huk9uri@gmail.com>
2024-10-31 14:37:06 +0900

    [#279] 30 / 23028kb 316ms / 각 자릿수 더해서 3의 배수면 그 수의 모든 조합은 3의 배수래요 ;;

--- slowHuk9/BOJ/src/BOJ_10610.java
+++ slowHuk9/BOJ/src/BOJ_10610.java
@@ -2,53 +2,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BOJ_10610 {
-	static long max = -2;
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		String N = sc.next();
-		int number = Integer.parseInt(N);
-		int length = N.length();
-		int[] num = new int[length];
-		for (int i = length - 1; i >= 0; i--) {
-			num[i] = (int) (number / Math.pow(10, i));
-			number %= Math.pow(10, i);
-		} // 자릿수 분리 완료
-
-		Arrays.sort(num);
-		boolean[] visited = new boolean[length];
-		int[] output = new int[length];
-
-		perm(num, output, 0, length, visited);
-		System.out.println(max);
-	}
-
-	static void perm(int[] num, int[] output, int depth, int length, boolean[] visited) {
-		if (depth == length) {
-			long result = solve(output);
-			max = Math.max(max, result);
-			return;
-		}
-
-		for (int i = output.length - 1; i >= 0; i--) {
-			if (!visited[i]) {
-				output[depth] = num[i];
-				visited[i] = true;
-				perm(num, output, depth + 1, length, visited);
-				visited[i] = false;
+		
+		int[] digits = new int[N.length()];
+		
+		int sum = 0;
+		boolean hasZero = false;
+		for (int i = 0; i < digits.length; i++) {
+			digits[i] = N.charAt(i) -'0';
+			sum += digits[i];
+			if(digits[i] == 0) {
+				hasZero = true;
 			}
 		}
-	}
-
-	static long solve(int[] output) {
-		long tmp = 0;
-		for (int i = 0; i < output.length; i++) {
-			tmp += output[i] * Math.pow(10, output.length - 1 - i);
+		
+		if(sum % 3 == 0 && hasZero) {
+			Arrays.sort(digits);
+			StringBuilder target = new StringBuilder();
+			for (int i = digits.length-1; i >= 0; i--) {
+				target.append(digits[i]);
+			}
+			System.out.println(target.toString());
+			return;
+		}else {
+			System.out.println(-1);
+			return;
 		}
-		if (tmp % 30 == 0) {
-			return tmp;
-		}
-		return -1;
 	}
+	
 }
